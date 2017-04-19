4cae247bcd60b7c0e609c128a4ca34d37d88c957
Andrii Melnichuk <andrewlutsk88@gmail.com>
2017-04-19 17:40:40 +0300

    make main page

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,13 @@
 $(function(){
 
+    $(".owl-carousel").owlCarousel({
+        autoWidth:true,
+        items:4,
+        nav:true,
+        loop: true,
+        autoPlay: 4000,
+        dots : false,
+        navText : ["",""],
+    });
+
 });
