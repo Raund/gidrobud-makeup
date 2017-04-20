1d310c49c18fa7acce7c79f1a51a639939fa58b9
Andrii Melnichuk <andrewlutsk88@gmail.com>
2017-04-20 18:27:07 +0300

    make license page and fix other

--- app/js/common.js
+++ app/js/common.js
@@ -10,4 +10,20 @@ $(function(){
         navText : ["",""],
     });
 
+    $('.flex-gallery').each(function(){
+        $(this).unitegallery({
+            gallery_theme: "tilesgrid",
+            gallery_width:"100%",              //gallery width
+            grid_space_between_cols:15,
+            grid_space_between_rows:5,
+            grid_space_between_mobile:0,
+            tile_enable_border:false,
+            tile_enable_shadow:false,
+            grid_padding:0,
+            tile_width: 255,						//tile width
+            tile_height: 365,
+            grid_num_rows:10
+        });
+    });
+
 });
